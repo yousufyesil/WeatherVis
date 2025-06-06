b4376818f4bddfcb452996f2267d5213e6def3ca
Yousuf Yesil <yousufyesil@icloud.com>
2025-06-06 15:02:01 +0200

    Inital Commit

---
+++ plot.js
@@ -0,0 +1,33 @@
+function makeplot() {
+    d3.csv("./Data/air_temperature_mean/regional_averages_tm_year.csv", function(data){ processData(data) } );
+
+};
+
+function processData(allRows) {
+
+    console.log(allRows);
+    var x = [], y = [], standard_deviation = [];
+
+    for (var i=0; i<allRows.length; i++) {
+        row = allRows[i];
+        x.push( row['AAPL_x'] );
+        y.push( row['AAPL_y'] );
+    }
+    console.log( 'X',x, 'Y',y, 'SD',standard_deviation );
+    makePlotly( x, y, standard_deviation );
+}
+
+function makePlotly( x, y, standard_deviation ){
+    var plotDiv = document.getElementById("plot");
+    var traces = [{
+        x: x,
+        y: y
+    }];
+
+    Plotly.newPlot('myDiv', traces, {
+        title: {
+            text: 'Plotting CSV data from AJAX call'
+        }
+    });
+};
+makeplot();
